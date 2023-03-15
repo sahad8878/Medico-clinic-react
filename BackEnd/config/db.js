4f011cdd5fa305d3a93982bcaa244bef48f9dfb0
sahad <msahadpp@gmail.com>
2023-03-15 12:09:04 +0530

    port resolve

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -4,8 +4,8 @@ const colors = require('colors');
 
 const connectDB = async () => {
   try {
-    // await mongoose.connect(process.env.DATABASE);
-    await mongoose.connect(process.env.LOCAL_DATABASE)
+    await mongoose.connect(process.env.DATABASE);
+    // await mongoose.connect(process.env.LOCAL_DATABASE)
     console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
     console.log(`Mongodb server issue ${error}`.bgRed.white);
